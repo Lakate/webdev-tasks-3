90a9de009ceecdb1f9682d309ec485edb8939bd0
Kate Latypova <katelat3009@gmail.com>
2016-03-16 21:57:45 +0500

    добавила асинхронность + исправила все на should интерфейс

--- test/flow-test.js
+++ test/flow-test.js
@@ -2,25 +2,25 @@
 
 const flow = require('../lib/flow.js');
 const should = require('chai').should();
-const expect = require('chai').expect;
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 require('chai').use(sinonChai);
 
 describe('Testing flow.js', () => {
     describe('Tests for serial function', () => {
-        it('should return error if got not Array of funcs', () => {
-            var callback = sinon.spy();
+        it('should throw error if got not Array of funcs', () => {
+            const callback = sinon.spy();
 
             flow.serial({}, callback);
-            expect(callback.calledOnce).to.be.true;
-            expect(callback).calledWith('Wrong arguments!');
+            callback.should.have.been.calledOnce;
+            callback.should.have.been.calledWith(new Error('Wrong arguments!'));
         });
 
         it('should call callback at once if got []', () => {
-            var callback = sinon.spy();
+            const callback = sinon.spy();
+
             flow.serial(([]), callback);
-            expect(callback.calledOnce).to.be.true;
+            callback.should.have.been.calledOnce;
         });
 
         it('should return [] if got []', () => {
@@ -30,234 +30,240 @@ describe('Testing flow.js', () => {
             });
         });
 
-        it('should call callback at once if got 1 function to do', () => {
-            var func = sinon.spy(cb => {
-                cb(null, 1);
+        it('should call callback at once if got 1 function to do', done => {
+            const func = sinon.spy(cb => {
+                setTimeout(() => cb(null, 1), 0);
             });
-            var callback = sinon.spy();
+            const callback = sinon.spy((err, data) => {
+                callback.should.have.been.calledOnce;
+                callback.should.have.been.calledAfter(func);
+                callback.should.have.been.calledWith(null, 1);
+                done(err);
+            });
 
             flow.serial([func], callback);
-            expect(func).to.have.been.calledOnce;
-            expect(callback).to.have.been.calledOnce;
-            expect(callback).to.have.been.calledWithExactly(null, 1);
+            func.should.have.been.calledOnce;
         });
 
-        it('should call functions at once if got 2 function to do', () => {
-            var func1 = sinon.spy(cb => {
-                cb(null, 1);
+        it('should call functions at once if got 2 function to do', done => {
+            const func1 = sinon.spy(cb => {
+                setTimeout(cb(null, 1), 0);
             });
-            var func2 = sinon.spy((data, cb) => {
-                cb(null, 2);
+            const func2 = sinon.spy((data, cb) => {
+                setTimeout(cb(null, 2), 1000);
             });
-            var callback = sinon.spy();
+            const callback = sinon.spy(() => done());
 
             flow.serial([func1, func2], callback);
-            expect(func1).to.have.been.calledOnce;
-            expect(func2).to.have.been.calledOnce;
-            expect(func2.args[0][0]).to.be.equal(1);
+            func1.should.have.been.calledOnce;
+            func2.should.have.been.calledOnce;
+            func2.should.have.been.calledWith(1);
         });
 
-        it('should call callback at once if got 2 function to do', () => {
-            var func1 = sinon.spy(cb => {
-                cb(null, 1);
+        it('should call callback at once if got 2 function to do', done => {
+            const func1 = sinon.spy(cb => {
+                setTimeout(cb(null, 1), 0);
             });
-            var func2 = sinon.spy((data, cb) => {
-                cb(null, 2);
+            const func2 = sinon.spy((data, cb) => {
+                setTimeout(cb(null, 2), 1000);
             });
-            var callback = sinon.spy();
+            const callback = sinon.spy(() => done());
 
             flow.serial([func1, func2], callback);
-            expect(callback).to.have.been.calledOnce;
-            expect(callback).to.have.been.calledWithExactly(null, 2);
-            expect(callback.calledAfter(func2)).to.be.true;
+            callback.should.have.been.calledOnce;
+            callback.should.have.been.calledWith(null, 2);
+            callback.should.have.been.calledAfter(func2);
         });
 
-        it('should second function call after first function', () => {
-            var func1 = sinon.spy(cb => {
-                cb(null, 1);
+        it('should second function call after first function', done => {
+            const func1 = sinon.spy(cb => {
+                setTimeout(cb(null, 1), 0);
             });
-            var func2 = sinon.spy((data, cb) => {
-                cb(null, 2);
+            const func2 = sinon.spy((data, cb) => {
+                setTimeout(cb(null, 2), 1000);
             });
-            var callback = sinon.spy();
+            const callback = sinon.spy(() => done());
 
             flow.serial([func1, func2], callback);
-            expect(func2.calledAfter(func1)).to.be.true;
+            func2.should.have.been.calledAfter(func1);
         });
 
-        it('should not call 2d function call after first function, if first throw error', () => {
-            var func1 = sinon.spy(cb => {
-                cb('err');
+        it('should not call 2d function call after first function, if first throw error', done => {
+            const func1 = sinon.spy(cb => {
+                setTimeout(cb(new Error('err')), 0);
             });
-            var func2 = sinon.spy((data, cb) => {
-                cb(null, 2);
+            const func2 = sinon.spy((data, cb) => {
+                setTimeout(cb(null, 2), 1000);
             });
-            var callback = sinon.spy();
+            const callback = sinon.spy(() => done());
 
             flow.serial([func1, func2], callback);
-            expect(func1).to.have.been.calledOnce;
-            expect(func2).to.not.have.been.called;
-            expect(callback).to.have.been.calledOnce;
-            expect(callback.args[0][0]).to.be.equal('err');
+            func1.should.have.been.calledOnce;
+            func2.should.not.have.been.called;
+            callback.should.have.been.calledOnce;
+            callback.should.have.been.calledWith(new Error('err'));
         });
     });
     describe('Tests for parallel function', function () {
         it('should return error if got not Array of funcs', () => {
-            var callback = sinon.spy();
+            const callback = sinon.spy();
 
             flow.parallel({}, callback);
-            expect(callback.calledOnce).to.be.true;
-            expect(callback).calledWith('Wrong arguments!');
+            callback.should.have.been.calledOnce;
+            callback.should.have.been.calledWith(new Error('Wrong arguments!'));
         });
 
         it('should call callback at once if got []', () => {
-            var callback = sinon.spy();
+            const callback = sinon.spy();
+
             flow.parallel(([]), callback);
-            expect(callback.calledOnce).to.be.true;
-            expect(callback.args[0][1]).to.be.an.instanceOf(Array);
+            callback.should.have.been.calledOnce;
+            callback.args[0][1].should.be.an.instanceOf(Array);
         });
 
-        it('should call callback at once after functions', () => {
-            var func1 = sinon.spy(cb => {
-                cb(null, 1);
+        it('should call callback at once after functions', done => {
+            const func1 = sinon.spy(cb => {
+                setTimeout(cb(null, 1), 0);
             });
-            var func2 = sinon.spy(cb => {
-                cb(null, 2);
+            const func2 = sinon.spy(cb => {
+                setTimeout(cb(null, 2), 1000);
             });
-            var callback = sinon.spy((err, data) => {
-                expect(callback).to.have.been.calledOnce;
-                expect(callback.calledAfter(func1)).to.be.true;
-                expect(callback.calledAfter(func2)).to.be.true;
+            const callback = sinon.spy((err, data) => {
+                callback.should.have.been.calledOnce;
+                callback.should.have.been.calledAfter(func1);
+                callback.should.have.been.calledAfter(func2);
+                done();
             });
 
             flow.parallel([func1, func2], callback);
-            expect(func1).to.have.been.calledOnce;
-            expect(func2).to.have.been.calledOnce;
+            func1.should.have.been.calledOnce;
+            func2.should.have.been.calledOnce;
         });
 
-        it('should call callback with right data array', () => {
-            var func1 = sinon.spy(cb => {
-                cb(null, 1);
+        it('should call callback with right data array', done => {
+            const func1 = sinon.spy(cb => {
+                setTimeout(cb(null, 1), 0);
             });
-            var func2 = sinon.spy(cb => {
-                cb(null, 2);
+            const func2 = sinon.spy(cb => {
+                setTimeout(cb(null, 2), 1000);
             });
-            var callback = sinon.spy((err, data) => {
-                expect(callback).to.have.been.calledOnce;
-                expect(callback).to.be.calledWith(undefined, [1, 2]);
+            const callback = sinon.spy((err, data) => {
+                callback.should.have.been.calledOnce;
+                callback.should.have.been.calledWith(undefined, [1, 2]);
+                done();
             });
 
             flow.parallel([func1, func2], callback);
-            expect(func1).to.have.been.calledOnce;
-            expect(func2).to.have.been.calledOnce;
         });
 
         it('should return error if one of functions throw err', () => {
-            var func1 = sinon.spy(cb => {
-                cb('err');
+            const func1 = sinon.spy(cb => {
+                setTimeout(cb(new Error('err')), 0);
             });
-            var func2 = sinon.spy(cb => {
-                cb(null, 2);
+            const func2 = sinon.spy(cb => {
+                setTimeout(cb(null, 2), 100);
             });
-            var callback = sinon.spy((err, data) => {
-                expect(callback).to.have.been.calledOnce;
-                expect(callback.calledAfter(func1)).to.be.true;
-                expect(callback.calledAfter(func2)).to.be.true;
-                expect(callback).to.be.calledWith('err', [undefined, 2]);
+            const callback = sinon.spy((err, data) => {
+                callback.should.have.been.calledWith(new Error('err'), [undefined, 2]);
             });
 
             flow.parallel([func1, func2], callback);
-            expect(func1).to.have.been.calledOnce;
-            expect(func2).to.have.been.calledOnce;
         });
 
-        it('should call all functions parallel if limit bigger then count of funcs', () => {
-            var func1 = sinon.spy(cb => {
-                cb(null, 1);
+        it('should call all functions parallel if limit bigger then count of funcs', done => {
+            const func1 = sinon.spy(cb => {
+                setTimeout(cb(null, 1), 500);
             });
-            var func2 = sinon.spy(cb => {
-                cb(null, 2);
+            const func2 = sinon.spy(cb => {
+                setTimeout(cb(null, 2), 1000);
             });
-            var callback = sinon.spy((err, data) => {
-                expect(callback).to.have.been.calledOnce;
-                expect(callback).to.be.calledWith(undefined, [1, 2]);
+            const callback = sinon.spy((err, data) => {
+                callback.should.have.been.calledOnce;
+                callback.should.have.been.calledWith(undefined, [1, 2]);
+                done();
             });
 
             flow.parallel([func1, func2], 10, callback);
-            expect(func1).to.have.been.calledOnce;
-            expect(func2).to.have.been.calledOnce;
+            func1.should.have.been.calledOnce;
+            func2.should.have.been.calledOnce;
         });
 
-        it('should call functions sync if limit equal 1', () => {
-            var func1 = sinon.spy(cb => {
-                cb(null, 1);
+        it('should call functions sync if limit equal 1', done => {
+            const func1 = sinon.spy(cb => {
+                setTimeout(cb(null, 1), 1000);
             });
-            var func2 = sinon.spy(cb => {
-                cb(null, 2);
+            const func2 = sinon.spy(cb => {
+                setTimeout(cb(null, 2), 500);
             });
-            var callback = sinon.spy((err, data) => {
-                expect(callback).to.have.been.calledOnce;
-                expect(callback).to.be.calledWith(undefined, [1, 2]);
+            const callback = sinon.spy((err, data) => {
+                callback.should.have.been.calledOnce;
+                callback.should.have.been.calledAfter(func2);
+                callback.should.have.been.calledWith(undefined, [1, 2]);
+                done();
             });
 
             flow.parallel([func1, func2], 1, callback);
-            expect(func1).to.have.been.calledOnce;
-            expect(func2).to.have.been.calledOnce;
-            expect(func2.calledAfter(func1)).to.be.true;
+            func1.should.have.been.calledOnce;
+            func2.should.have.been.calledOnce;
+            func2.should.have.been.calledAfter(func1);
         });
 
-        it('should return [] if limit equal 0', () => {
-            var func1 = sinon.spy(cb => {
-                cb(null, 1);
+        it('should return [] if limit equal 0', done => {
+            const func1 = sinon.spy(cb => {
+                setTimeout(cb(null, 1), 1000);
             });
-            var func2 = sinon.spy(cb => {
-                cb(null, 2);
+            const func2 = sinon.spy(cb => {
+                setTimeout(cb(null, 2), 1000);
             });
-            var callback = sinon.spy((err, data) => {
-                expect(callback).to.have.been.calledOnce;
-                expect(callback).to.be.calledWith(undefined, []);
+            const callback = sinon.spy((err, data) => {
+                callback.should.have.been.calledOnce;
+                callback.should.have.been.calledWith(undefined, []);
+                done();
             });
 
             flow.parallel([func1, func2], 0, callback);
-            expect(func1).to.not.have.been.calledOnce;
-            expect(func2).to.not.have.been.calledOnce;
+            func1.should.not.have.been.called;
+            func2.should.not.have.been.called;
         });
     });
     describe('Tests for map function', function () {
         it('should return error if got not Array of funcs', () => {
-            var func = () => {};
-            var callback = sinon.spy();
+            const func = () => {};
+            const callback = sinon.spy();
 
             flow.map({}, func, callback);
-            expect(callback.calledOnce).to.be.true;
-            expect(callback).calledWith('Wrong arguments!');
+            callback.should.have.been.calledOnce;
+            callback.should.have.been.calledWith(new Error('Wrong arguments!'));
         });
 
         it('should call callback at once if got []', () => {
-            var func = () => {};
-            var callback = sinon.spy();
+            const func = () => {};
+            const callback = sinon.spy();
+
             flow.map(([]), func, callback);
-            expect(callback.calledOnce).to.be.true;
-            expect(callback).to.be.calledWith(undefined, []);
+            callback.should.have.been.calledOnce;
+            callback.should.have.been.calledWith(undefined, []);
         });
 
-        it('should call callback at once after functions', () => {
-            var func = sinon.spy((data, cb) => {
-                cb(null, data);
+        it('should call callback at once after functions', done => {
+            const func = sinon.spy((data, cb) => {
+                setTimeout(cb(null, data), 100);
             });
-            var callback = sinon.spy((err, data) => {
-                expect(callback).to.have.been.calledOnce;
+            const callback = sinon.spy((err, data) => {
+                callback.should.have.been.calledOnce;
+                done();
             });
 
             flow.map([1, 2], func, callback);
         });
 
-        it('should apply function to each param', () => {
-            var func = sinon.spy((data, cb) => {
-                cb(null, data + 1);
+        it('should apply function to each param', done => {
+            const func = sinon.spy((data, cb) => {
+                setTimeout(cb(null, data + 1), 100);
             });
-            var callback = sinon.spy((err, data) => {
-                expect(callback).to.be.calledWith(undefined, [2, 3]);
+            const callback = sinon.spy((err, data) => {
+                callback.should.have.been.calledWith(undefined, [2, 3]);
+                done();
             });
 
             flow.map([1, 2], func, callback);
@@ -265,22 +271,23 @@ describe('Testing flow.js', () => {
     });
     describe('Tests for makeAsync function', function () {
         it('should return a function', () => {
-            var syncFunction = () => {};
+            const syncFunction = () => {};
 
             flow.makeAsync(syncFunction(1));
             syncFunction.should.be.a('function');
         });
 
-        it('should call callback', () => {
-            var syncFunction = (arg) => {
+        it('should call callback', done => {
+            const syncFunction = (arg) => {
                 return ++arg;
             };
-            var callback = sinon.spy((err, data) => {
-                expect(callback).to.have.been.calledOnce;
-                expect(callback).to.have.been.calledWithExactly(undefined, 1);
+            const callback = sinon.spy((err, data) => {
+                callback.should.have.been.calledOnce;
+                callback.should.have.been.calledWith(undefined, 1);
+                done();
             });
 
-            var asyncFunction = flow.makeAsync(syncFunction);
+            const asyncFunction = flow.makeAsync(syncFunction);
             asyncFunction(0, callback);
         });
     });
